3fe01ccc68ad29f037ac0c5ade6f33a5e39c4c6f
mariam <magdymariam3@gmail.com>
2019-04-01 23:06:36 +0200

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment.prod';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB7gvbqR2MpmQuqqCQ700w2v0jWtKMB1uQ",
+    authDomain: "crud-be1e2.firebaseapp.com",
+    databaseURL: "https://crud-be1e2.firebaseio.com",
+    projectId: "crud-be1e2",
+    storageBucket: "crud-be1e2.appspot.com",
+    messagingSenderId: "564477320873"
+  }
 };
